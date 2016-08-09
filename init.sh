1e9bec6faf8714a79c79f0df87c6fe745be5b94f
johnmarinelli <john.marinelli.dev@gmail.com>
2016-08-09 13:10:46 -0700

    fix init

--- init.sh
+++ init.sh
@@ -4,8 +4,8 @@ echo "Cloning my fork of clojure-hadoop..."
 git clone https://github.com/johnmarinelli/clojure-hadoop
 echo "Compiling"
 export LEIN_ROOT=$USER
-cd clojure-hadoop && \
-lein deps && \
-lein uberjar && \ 
-mv target/examples.jar . && \
+cd clojure-hadoop 
+lein deps 
+lein uberjar 
+mv target/examples.jar . 
 java -cp examples.jar clojure_hadoop.examples.julia julia/julia.txt julia/julia2.txt julia/julia3.txt julia/julia4.txt outj
